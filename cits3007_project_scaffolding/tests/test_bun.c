e507050065b189611dcdb95d3075cc3bea338784
Nicolas Hasleby <24314307@student.uwa.edu.au>
2026-04-27 01:00:38 +0000

    adding skeleton tests

--- cits3007_project_scaffolding/tests/test_bun.c
+++ cits3007_project_scaffolding/tests/test_bun.c
@@ -63,6 +63,7 @@ START_TEST(test_valid_binar_asset){}END_TEST
 START_TEST(test_valid_multi_asset_stack){}END_TEST
 START_TEST(test_valid_rle){}END_TEST
 
+
 START_TEST(test_bad_magic) {
     BunParseContext ctx = {0};
     BunHeader header    = {0};
